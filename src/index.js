53cef602ef7c86327fac9d05e317655d1581f7a1
Alexandre Uzan <alexandreuzan9@gmail.com>
2025-09-12 14:25:02 +0200

    Update for ?address=http://core-ip:core-port surppr

--- src/index.js
+++ src/index.js
@@ -9,21 +9,29 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import RestreamerUI from './RestreamerUI';
 
-let address = window.location.protocol + '//' + window.location.host;
+// Détermine l'adresse par défaut à partir de la variable d'environnement
+// fournie au build/au conteneur, avec repli sur l'origine actuelle
+let address = process.env.REACT_APP_DEFAULT_CORE_ADDRESS || (window.location.protocol + '//' + window.location.host);
+if (!address) {
+  address = window.location.protocol + '//' + window.location.host;
+}
+
+// Si l'application est servie depuis /ui/, on enlève le suffixe pour cibler la racine Core
 if (window.location.pathname.endsWith('/ui/')) {
-	address += window.location.pathname.replace(/ui\/$/, '');
+  address += window.location.pathname.replace(/ui\/$/, '');
 }
 
 const urlParams = new URLSearchParams(window.location.search.substring(1));
 if (urlParams.has('address') === true) {
-	address = urlParams.get('address');
+  // Le paramètre d'URL (si présent) reste prioritaire
+  address = urlParams.get('address');
 }
 
 createRoot(document.getElementById('root')).render(
-	<StyledEngineProvider injectFirst>
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<RestreamerUI address={address} />
-		</ThemeProvider>
-	</StyledEngineProvider>
+  <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RestreamerUI address={address} />
+    </ThemeProvider>
+  </StyledEngineProvider>
 );
