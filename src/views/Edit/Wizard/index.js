cce373bbd837b59b22677ed36dd41ff3642ad227
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2026-09-22 10:36:20 +0200

    fix: preserve custom audio source in wizard

--- src/views/Edit/Wizard/index.js
+++ src/views/Edit/Wizard/index.js
@@ -597,14 +597,20 @@ export default function Wizard(props) {
 			const value = event.target.value;
 
 			const profile = $profile.audio;
+			const custom = $profile.custom;
 			let source = null;
 
 			if (value === 'video') {
 				profile.source = 0;
+				custom.selected = false;
+				custom.stream = profile.stream;
 
 				source = null;
 			} else if (value === 'alsa') {
 				profile.source = 1;
+				profile.stream = -1;
+				custom.selected = true;
+				custom.stream = -2;
 
 				// The first ALSA device is selected by default
 				let address = '';
@@ -624,6 +630,9 @@ export default function Wizard(props) {
 				source.inputs = fullSource.func.createInputs(source.settings);
 			} else if (value === 'silence') {
 				profile.source = 1;
+				profile.stream = -1;
+				custom.selected = true;
+				custom.stream = -2;
 
 				source = M.initSource('audio', null);
 
@@ -638,6 +647,9 @@ export default function Wizard(props) {
 				source.inputs = fullSource.func.createInputs(source.settings);
 			} else {
 				profile.source = -1;
+				profile.stream = -1;
+				custom.selected = false;
+				custom.stream = -1;
 
 				source = null;
 			}
@@ -650,6 +662,7 @@ export default function Wizard(props) {
 			setProfile({
 				...$profile,
 				audio: profile,
+				custom: custom,
 			});
 		};
 
